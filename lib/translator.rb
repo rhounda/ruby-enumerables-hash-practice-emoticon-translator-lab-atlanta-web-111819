e9da7663a7a3cd062c5539170e26655e493f7054
Bryan Rhodunda <rhodunda@udel.edu>
2019-11-17 20:20:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,7 +10,7 @@ def load_library(path)
   emontican_hash['get_meaning'] = hash.new
 
 emoticons.each do |english_word, emonticon_set|
-emoticon_hash["get_emoticon"][emoticon_set.first emoticon_set.last
+emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
     emoticon_hash["get_meaning"][emoticon_set.last] = english_word
 
 end
