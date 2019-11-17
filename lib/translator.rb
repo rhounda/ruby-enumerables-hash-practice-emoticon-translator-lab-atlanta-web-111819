7ff7a586880883dd17a3a0c190b7e6d5288a074d
Bryan Rhodunda <rhodunda@udel.edu>
2019-11-17 20:22:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,10 +16,15 @@ end
 emoticon_hash
 
 
-def get_japanese_emoticon
- 
+def get_japanese_emoticon(path, emoticon)
+ emoticon_hash = load_library(path)
+  result = emoticon_hash["get_emoticon"][emoticon]
+  if result == nil
+    result = "Sorry, that emoticon was not found" 
+  end
+  result
 end
 
 def get_english_meaning
-  # code goes here
+ 
 end
