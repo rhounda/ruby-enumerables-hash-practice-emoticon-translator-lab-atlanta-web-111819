d84742e6da9cff8519fdc37b26bd28dfb90395bc
Bryan Rhodunda <rhodunda@udel.edu>
2019-11-17 20:16:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,7 @@ def load_library(path)
   emoticons = yaml.load_file(path)
   emoticon_hash = hash.new
   
-  emoticon_hash["get_emon"]
+  emoticon_hash["get_emoticon"]
 end
 
 def get_japanese_emoticon
